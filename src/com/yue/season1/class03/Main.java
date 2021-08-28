51a1b99abecf96de1523ed96f809a9a091c2d62b
2yue <yue-1227@outlook.com>
2021-08-28 22:02:32 +0800

    update: 更新链表结构为双向链表并追加了基本测试。

--- src/com/yue/season1/class03/Main.java
+++ src/com/yue/season1/class03/Main.java
@@ -1,8 +1,45 @@
 package com.yue.season1.class03;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
+        updateTest();
+    }
 
+    private static void updateTest() {
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        list.add(0);
+        list.add(1);
+        list.add(0, -2);
+        list.add(1, -1);
+
+        // size=4, [-2, -1, 0, 1]
+        System.out.println(list);
+
+        // -2
+        System.out.println(list.getFirst());
+
+        // 1
+        System.out.println(list.getLast());
+
+
+        list.remove(0);
+        list.remove(list.size() - 1);
+        list.add(1);
+        list.remove(1);
+
+        // size=2, [-1, 1]
+        System.out.println(list);
+
+        list.addFirst(-2);
+        list.addLast(2);
+
+        // size=4, [-2, -1, 1, 2]
+        System.out.println(list);
+    }
+
+    private static void basicTest() {
         MyLinkedList<Integer> list = new MyLinkedList<>();
         list.add(0);
         list.add(1);
--- src/com/yue/season1/class03/MyLinkedList.java
+++ src/com/yue/season1/class03/MyLinkedList.java
@@ -2,20 +2,25 @@ package com.yue.season1.class03;
 
 import com.yue.season1.class02.MyAbstractList;
 
+import java.util.NoSuchElementException;
+
 /**
  * @author 19745
  */
 public class MyLinkedList<E> extends MyAbstractList<E> {
 
     private Node<E> firstNode;
+    private Node<E> lastNode;
 
     private static class Node<E> {
         E element;
         Node<E> nextNode;
+        Node<E> prevNode;
 
-        Node (E element, Node<E> nextNode) {
+        Node (Node<E> prevNode, E element, Node<E> nextNode) {
             this.element = element;
             this.nextNode = nextNode;
+            this.prevNode = prevNode;
         }
     }
 
@@ -23,6 +28,7 @@ public class MyLinkedList<E> extends MyAbstractList<E> {
     public void clear() {
         size = 0;
         firstNode = null;
+        lastNode = null;
     }
 
     @Override
@@ -43,27 +49,18 @@ public class MyLinkedList<E> extends MyAbstractList<E> {
     @Override
     public void add(int index, E element) {
         rangeCheckForAdd(index);
-        if (index == 0) {
-            firstNode = new Node<>(element, firstNode);
+        if (index == size) {
+            addLast(element);
         } else {
-            Node<E> prevNode = getNode(index - 1);
-            prevNode.nextNode = new Node<>(element, prevNode.nextNode);
+            addBefore(element, getNode(index));
         }
-        size += 1;
     }
 
     @Override
     public E remove(int index) {
         rangeCheck(index);
-        E oldElement = firstNode.element;
-        if (index == 0) {
-            firstNode = firstNode.nextNode;
-        } else {
-            Node<E> prevNode = getNode(index - 1);
-            oldElement = prevNode.nextNode.element;
-            prevNode.nextNode = prevNode.nextNode.nextNode;
-        }
-        size -= 1;
+        Node<E> removeNode = getNode(index);
+        E oldElement = unlink(removeNode);
         return oldElement;
     }
 
@@ -86,6 +83,22 @@ public class MyLinkedList<E> extends MyAbstractList<E> {
         return ELEMENT_NOT_FOUND;
     }
 
+    public E getFirst() {
+        final Node<E> first = firstNode;
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+        return first.element;
+    }
+
+    public E getLast() {
+        final Node<E> last = lastNode;
+        if (last == null) {
+            throw new NoSuchElementException();
+        }
+        return last.element;
+    }
+
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
@@ -102,17 +115,86 @@ public class MyLinkedList<E> extends MyAbstractList<E> {
         return stringBuilder.toString();
     }
 
+    public void addFirst(E element) {
+        final Node<E> f = firstNode;
+        final Node<E> newNode = new Node<>(null, element, firstNode);
+        firstNode = newNode;
+
+        if (f == null) {
+            lastNode = newNode;
+        } else {
+            f.prevNode = newNode;
+        }
+        size += 1;
+    }
+
+    public void addLast(E element) {
+        final Node<E> l = lastNode;
+        final Node<E> newNode = new Node<>(lastNode, element, null);
+        lastNode = newNode;
+
+        if (l == null) {
+            firstNode = newNode;
+        } else {
+            l.nextNode = newNode;
+        }
+        size += 1;
+    }
+
+
+
     /**
      * 获取指定索引对应的节点
      * @param index 索引
      * @return 节点
      */
     private Node<E> getNode(int index) {
-        Node<E> node = firstNode;
-        for (int i = 0; i < index; i ++) {
-            node = node.nextNode;
+        Node<E> node;
+        if (index < size >> 1) {
+            node = firstNode;
+            for (int i = 0; i < index; i ++) {
+                node = node.nextNode;
+            }
+        } else {
+            node = lastNode;
+            for (int i = size - 1; i > index; i --) {
+                node = node.prevNode;
+            }
         }
         return node;
     }
 
+    private void addBefore(E element, Node<E> node) {
+
+        final Node<E> prevNode = node.prevNode;
+        final Node<E> newNode = new Node<>(prevNode, element, node);
+        node.prevNode = newNode;
+        if (prevNode == null) {
+            firstNode = newNode;
+        } else {
+            prevNode.nextNode = newNode;
+        }
+        size += 1;
+    }
+
+    private E unlink(Node<E> node) {
+        E element = node.element;
+        Node<E> prevNode = node.prevNode;
+        Node<E> nextNode = node.nextNode;
+
+        if (prevNode == null) {
+            firstNode = nextNode;
+        } else {
+            prevNode.nextNode = nextNode;
+        }
+
+        if (nextNode == null) {
+            lastNode = prevNode;
+        } else {
+            nextNode.prevNode = prevNode;
+        }
+
+        size -= 1;
+        return element;
+    }
 }
